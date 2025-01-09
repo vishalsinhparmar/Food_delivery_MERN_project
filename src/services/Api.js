ffd5f8493bb7287f00b19ca81e84784294903f3c
vishalsinh <vishalsinhparmar821@gamil.com>
2025-01-10 02:10:58 +0530

    urlapi

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios  from "axios";
 
 
 const api = axios.create({
-    baseURL:"http://localhost:8000/api"
+    baseURL:"https://foodapprestapi-production.up.railway.app/api"
 });
 
 api.interceptors.request.use(
